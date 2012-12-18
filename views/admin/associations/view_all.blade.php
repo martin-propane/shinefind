338724128afd7dc6616ee7ec0b2b9d1c09794673
Martin Tribo <martin@propanemedia.com>
2012-12-18 16:04:44 -0600

    Added view_all for Associations and Products views.

--- views/admin/associations/view_all.blade.php
+++ views/admin/associations/view_all.blade.php
@@ -7,27 +7,29 @@
 			<td>City</td>
 			<td>State</td>
 			<td>Zip</td>
-			<td>Phone</td>
 			<td>Email</td>
 			<td>Website</td>
-			<td>Corporate Address</td>
+			<td>Phone</td>
+			<td>Fax</td>
+			<td>Fee</td>
 			<td>Edit</td>
 		</tr>
 	</thead>
-	<?php foreach ($carwashes as $c): ?>
+	<?php foreach ($associations as $a): ?>
 	<tbody>
 		<tr>
-			<td><?php echo $c->id ?></td>
-			<td><?php echo $c->name ?></td>
-			<td><?php echo $c->busi_ad ?></td>
-			<td><?php echo $c->city ?></td>
-			<td><?php echo $c->state ?></td>
-			<td><?php echo $c->zip ?></td>
-			<td><?php echo $c->phone ?></td>
-			<td><?php echo $c->email ?></td>
-			<td><?php echo $c->website ?></td>
-			<td><?php echo $c->corp_ad ?></td>
-			<td><?php echo HTML::link_to_action('admin.carwashes@edit', 'Edit', array($c->id), array('class'=>'btn')) ?></td>
+			<td><?php echo $a->id ?></td>
+			<td><?php echo $a->name ?></td>
+			<td><?php echo $a->address ?></td>
+			<td><?php echo $a->city ?></td>
+			<td><?php echo $a->state ?></td>
+			<td><?php echo $a->zip ?></td>
+			<td><?php echo $a->email ?></td>
+			<td><?php echo $a->website ?></td>
+			<td><?php echo $a->phone ?></td>
+			<td><?php echo $a->fax ?></td>
+			<td><?php echo $a->fee ?></td>
+			<td><?php echo HTML::link_to_action('admin.associations@edit', 'Edit', array($a->id), array('class'=>'btn')) ?></td>
 		</tr>
 	</tbody>
 	<?php endforeach; ?>
--- views/admin/products/view_all.blade.php
+++ views/admin/products/view_all.blade.php
@@ -3,31 +3,21 @@
 		<tr>
 			<td>ID</td>
 			<td>Name</td>
-			<td>Address</td>
-			<td>City</td>
-			<td>State</td>
-			<td>Zip</td>
+			<td>Company</td>
 			<td>Phone</td>
-			<td>Email</td>
 			<td>Website</td>
-			<td>Corporate Address</td>
 			<td>Edit</td>
 		</tr>
 	</thead>
-	<?php foreach ($carwashes as $c): ?>
+	<?php foreach ($products as $p): ?>
 	<tbody>
 		<tr>
-			<td><?php echo $c->id ?></td>
-			<td><?php echo $c->name ?></td>
-			<td><?php echo $c->busi_ad ?></td>
-			<td><?php echo $c->city ?></td>
-			<td><?php echo $c->state ?></td>
-			<td><?php echo $c->zip ?></td>
-			<td><?php echo $c->phone ?></td>
-			<td><?php echo $c->email ?></td>
-			<td><?php echo $c->website ?></td>
-			<td><?php echo $c->corp_ad ?></td>
-			<td><?php echo HTML::link_to_action('admin.carwashes@edit', 'Edit', array($c->id), array('class'=>'btn')) ?></td>
+			<td><?php echo $p->id ?></td>
+			<td><?php echo $p->name ?></td>
+			<td><?php echo $p->company ?></td>
+			<td><?php echo $p->phone ?></td>
+			<td><?php echo $p->website ?></td>
+			<td><?php echo HTML::link_to_action('admin.products@edit', 'Edit', array($p->id), array('class'=>'btn')) ?></td>
 		</tr>
 	</tbody>
 	<?php endforeach; ?>
