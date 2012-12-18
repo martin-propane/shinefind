0c49e9386f5127507348850e21434734be905faa
Martin Tribo <martin@propanemedia.com>
2012-12-18 17:18:32 -0600

    Finished associations add and edit views.

--- views/admin/associations/add.blade.php
+++ views/admin/associations/add.blade.php
@@ -55,8 +55,8 @@ $state_list = array('AL'=>"Alabama",
 
 ?>
 <?php
-echo Form::open('admin/panel/add_location', 'POST', array('id' => 'addForm', 'class' => 'form-horizontal'));
-echo '<legend>Add location</legend>';
+echo Form::open('admin/associations/add', 'POST', array('id' => 'addForm', 'class' => 'form-horizontal'));
+echo '<legend>Add association</legend>';
 
 echo '<div class = "control-group">';
 echo Form::label('name', 'Name', array('class' => 'control-label'));
@@ -65,9 +65,9 @@ echo Form::text('name', null, array('class' => 'required'));
 echo '</div></div>';
 
 echo '<div class = "control-group">';
-echo Form::label('busi_ad', 'Address', array('class' => 'control-label'));
+echo Form::label('address', 'Address', array('class' => 'control-label'));
 echo '<div class = "controls">';
-echo Form::text('busi_ad');
+echo Form::text('address');
 echo '</div></div>';
 
 echo '<div class = "control-group">';
@@ -95,6 +95,13 @@ echo Form::text('phone', null, array('id' => 'phone', 'placeholder' => '123-456-
 echo '<span class = "help-inline" id = "phoneError"></span>';
 echo '</div></div>';
 
+echo '<div class = "control-group" id = "faxControl">';
+echo Form::label('fax', 'Fax', array('class' => 'control-label'));
+echo '<div class = "controls">';
+echo Form::text('fax', null, array('id' => 'fax', 'placeholder' => '123-456-7890'));
+echo '<span class = "help-inline" id = "faxError"></span>';
+echo '</div></div>';
+
 echo '<div class = "control-group">';
 echo Form::label('notes', 'Notes', array('class' => 'control-label'));
 echo '<div class = "controls">';
@@ -114,91 +121,9 @@ echo Form::text('website', null, array('class' => 'url', 'placeholder' => 'http:
 echo '</div></div>';
 
 echo '<div class = "control-group">';
-echo Form::label('corp_ad', 'Corporate Address', array('class' => 'control-label'));
+echo Form::label('fee', 'Fee', array('class' => 'control-label'));
 echo '<div class = "controls">';
-echo Form::text('corp_ad');
-echo '</div></div>';
-
-echo '<div class = "control-group">';
-echo Form::label('type', 'Type', array('class' => 'control-label'));
-echo '<div class = "controls">';
-echo '<div>';
-echo Form::checkbox('detailing', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('detailing', 'Detailing', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('freevac', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('freevac', 'Free Vacuums', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('fullservice', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('fullservice', 'Full Service', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('handwash', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('handwash', 'Hand Wash', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('mobile', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('mobile', 'Mobile', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('selfserve', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('selfserve', 'Self Serve', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('softtouch', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('softtouch', 'Soft Touch', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('touchfree', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('touchfree', 'Touch Free', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('tunnel', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('tunnel', 'Tunnel', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('xpress', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('xpress', 'Xpress', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '</div></div>';
-
-echo '<div class = "control-group">';
-echo Form::label('options', 'Options', array('class' => 'control-label'));
-echo '<div class = "controls">';
-echo '<div>';
-echo Form::checkbox('creditcards', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('creditcards', 'Credit Cards', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('conveniencestore', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('conveniencestore', 'Convenience Store', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('fuel', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('fuel', 'Fuel', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('giftcards', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('giftcards', 'Gift Cards', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('oilchange', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('oilchange', 'Oil Change', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('other_other', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('other_other', 'Other', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('petwash', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('petwash', 'Pet Wash', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('salon', TRUE, false, array('class' => 'checkbox'));
-echo Form::label('salon', 'Salon', array('class' => 'checkbox inline'));
-echo '</div>';
+echo Form::text('fee', null, array('placeholder' => '$1.00'));
 echo '</div></div>';
 
 echo '<div class = "control-group">';
@@ -223,6 +148,9 @@ $(document).ready(function()
 			name: 'required',
 			phone: {
 				validPhone: true
+			},
+			fax: {
+				validPhone: true
 			}
 		},
 		errorPlacement: function(error, element) {
@@ -240,11 +168,14 @@ $(document).ready(function()
 		errorElement: 'span'
 	});
 
-	$('input[name=phone]').change(function() {
-		var txtbox = $('input[name=phone]');
+	$('input[name=phone], input[name=fax]').change(function() {
+		var txtbox = $(this);
 		var num = txtbox.val().replace(/[^\d.]/g, '');
 		var newval = '';
-		if (num.length < 4) {
+		if (num.length == 0) {
+			newval = '';
+		}
+		else if (num.length < 4) {
 			newval = '(' + num;
 			if (num.length === 3)
 				newval = newval + ') ';
@@ -259,7 +190,6 @@ $(document).ready(function()
 
 		txtbox.val(newval);
 	});
-
 });
 </script>
 
--- views/admin/associations/edit.blade.php
+++ views/admin/associations/edit.blade.php
@@ -1,67 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Edit Location</title>
-<?php echo Asset::scripts(); ?>
-<?php echo Asset::styles(); ?>
-<?php echo HTML::script('js/jquery.validate.min.js'); ?>
-<script type = "text/javascript">
-$(document).ready(function()
-{
-	jQuery.validator.addMethod('validPhone', function (value, element) {
-		var num = value.replace(/[^\d.]/g, '');
-		return this.optional(element) || (num.length === 10);
-	}, 'Enter a valid phone number (123-456-7890).');
-
-	$('#addForm').validate({
-		onkeyup: false,
-		rules: {
-			name: 'required',
-			phone: {
-				validPhone: true
-			}
-		},
-		errorPlacement: function(error, element) {
-			error.insertAfter($(element));
-		},
-		highlight: function(element, errorClass, validClass) {
-			var con = $(element).parent().parent();
-			con.addClass(errorClass);
-
-		},
-		unhighlight: function(element, errorClass, validClass) {
-			var con = $(element).parent().parent();
-			con.removeClass(errorClass);
-		},
-		errorElement: 'span'
-	});
-
-	$('input[name=phone]').change(function() {
-		var txtbox = $('input[name=phone]');
-		var num = txtbox.val().replace(/[^\d.]/g, '');
-		var newval = '';
-		if (num.length < 4) {
-			newval = '(' + num;
-			if (num.length === 3)
-				newval = newval + ') ';
-		}
-		else if (num.length > 3 && num.length < 7) {
-			newval = '(' + num.substring(0, 3) + ') ' + num.substring(3);
-			if (num.length === 6)
-				newval = newval + ' - ';
-		}
-		else
-			newval = '(' + num.substring(0, 3) + ') ' + num.substring(3, 6) + ' - ' + num.substring(6);
-
-		txtbox.val(newval);
-	});
-
-});
-</script>
-</head>
-<body>
 <?php
-
 $state_list = array('AL'=>"Alabama",  
 			'AK'=>"Alaska",  
 			'AZ'=>"Arizona",  
@@ -113,21 +50,26 @@ $state_list = array('AL'=>"Alabama",
 			'WV'=>"West Virginia",  
 			'WI'=>"Wisconsin",  
 			'WY'=>"Wyoming");
+
+
+
 ?>
 <?php
-echo Form::open('admin/carwashes/edit/' . $id, 'POST', array('id' => 'addForm', 'class' => 'form-horizontal'));
+echo Form::open('admin/associations/edit/' . $id, 'POST', array('id' => 'addForm', 'class' => 'form-horizontal'));
+echo '<legend>Add association</legend>';
+
 echo Form::hidden('id', $id);
-echo '<legend>Edit location</legend>';
+
 echo '<div class = "control-group">';
-echo Form::label('name', 'Name', array('class' => 'control-label'), 'test');
+echo Form::label('name', 'Name', array('class' => 'control-label'));
 echo '<div class = "controls">';
 echo Form::text('name', $name, array('class' => 'required'));
 echo '</div></div>';
 
 echo '<div class = "control-group">';
-echo Form::label('busi_ad', 'Address', array('class' => 'control-label'));
+echo Form::label('address', 'Address', array('class' => 'control-label'));
 echo '<div class = "controls">';
-echo Form::text('busi_ad', $busi_ad);
+echo Form::text('address', $address);
 echo '</div></div>';
 
 echo '<div class = "control-group">';
@@ -149,10 +91,17 @@ echo Form::text('zip', $zip);
 echo '</div></div>';
 
 echo '<div class = "control-group" id = "phoneControl">';
-echo Form::label('phone', 'Phone', array('class' => 'control-label'));
+echo Form::label('phone1', 'Phone', array('class' => 'control-label'));
 echo '<div class = "controls">';
 echo Form::text('phone', $phone, array('id' => 'phone', 'placeholder' => '123-456-7890'));
 echo '<span class = "help-inline" id = "phoneError"></span>';
+echo '</div></div>';
+
+echo '<div class = "control-group" id = "faxControl">';
+echo Form::label('fax', 'Fax', array('class' => 'control-label'));
+echo '<div class = "controls">';
+echo Form::text('fax', $fax, array('id' => 'fax', 'placeholder' => '123-456-7890'));
+echo '<span class = "help-inline" id = "faxError"></span>';
 echo '</div></div>';
 
 echo '<div class = "control-group">';
@@ -174,91 +123,9 @@ echo Form::text('website', $website, array('class' => 'url', 'placeholder' => 'h
 echo '</div></div>';
 
 echo '<div class = "control-group">';
-echo Form::label('corp_ad', 'Corporate Address', array('class' => 'control-label'));
+echo Form::label('fee', 'Fee', array('class' => 'control-label'));
 echo '<div class = "controls">';
-echo Form::text('corp_ad', $corp_ad);
-echo '</div></div>';
-
-echo '<div class = "control-group">';
-echo Form::label('type', 'Type', array('class' => 'control-label'));
-echo '<div class = "controls">';
-echo '<div>';
-echo Form::checkbox('detailing', TRUE, $types['Detailing'], array('class' => 'checkbox'));
-echo Form::label('detailing', 'Detailing', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('freevac', TRUE, $types['FreeVacuums'], array('class' => 'checkbox'));
-echo Form::label('freevac', 'Free Vacuums', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('fullservice', TRUE, $types['FullService'], array('class' => 'checkbox'));
-echo Form::label('fullservice', 'Full Service', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('handwash', TRUE, $types['HandWash'], array('class' => 'checkbox'));
-echo Form::label('handwash', 'Hand Wash', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('mobile', TRUE, $types['Mobile'], array('class' => 'checkbox'));
-echo Form::label('mobile', 'Mobile', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('selfserve', TRUE, $types['SelfServe'], array('class' => 'checkbox'));
-echo Form::label('selfserve', 'Self Serve', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('softtouch', TRUE, $types['SoftTouch'], array('class' => 'checkbox'));
-echo Form::label('softtouch', 'Soft Touch', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('touchfree', TRUE, $types['TouchFree'], array('class' => 'checkbox'));
-echo Form::label('touchfree', 'Touch Free', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('tunnel', TRUE, $types['Tunnel'], array('class' => 'checkbox'));
-echo Form::label('tunnel', 'Tunnel', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('xpress', TRUE, $types['Xpress'], array('class' => 'checkbox'));
-echo Form::label('xpress', 'Xpress', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '</div></div>';
-
-echo '<div class = "control-group">';
-echo Form::label('options', 'Options', array('class' => 'control-label'));
-echo '<div class = "controls">';
-echo '<div>';
-echo Form::checkbox('creditcards', TRUE, $options['CreditCards'], array('class' => 'checkbox'));
-echo Form::label('creditcards', 'Credit Cards', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('conveniencestore', TRUE, $options['ConvenienceStore'], array('class' => 'checkbox'));
-echo Form::label('conveniencestore', 'Convenience Store', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('fuel', TRUE, $options['Fuel'], array('class' => 'checkbox'));
-echo Form::label('fuel', 'Fuel', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('giftcards', TRUE, $options['GiftCards'], array('class' => 'checkbox'));
-echo Form::label('giftcards', 'Gift Cards', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('oilchange', TRUE, $options['OilChange'], array('class' => 'checkbox'));
-echo Form::label('oilchange', 'Oil Change', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('other_other', TRUE, $options['Other'], array('class' => 'checkbox'));
-echo Form::label('other_other', 'Other', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('petwash', TRUE, $options['PetWash'], array('class' => 'checkbox'));
-echo Form::label('petwash', 'Pet Wash', array('class' => 'checkbox inline'));
-echo '</div>';
-echo '<div>';
-echo Form::checkbox('salon', TRUE, $options['Salon'], array('class' => 'checkbox'));
-echo Form::label('salon', 'Salon', array('class' => 'checkbox inline'));
-echo '</div>';
+echo Form::text('fee', $fee, array('placeholder' => '$1.00'));
 echo '</div></div>';
 
 echo '<div class = "control-group">';
@@ -266,7 +133,67 @@ echo '<div class = "controls">';
 echo Form::submit('Submit', array('class' => 'submit', 'id' => 'addSubmit', 'class' => 'btn'));
 echo '</div></div>';
 echo Form::close();
+
 ?>
-</body>
-</html>
+<?php echo HTML::script('js/jquery.validate.min.js'); ?>
+<script type = "text/javascript">
+$(document).ready(function()
+{
+	jQuery.validator.addMethod('validPhone', function (value, element) {
+		var num = value.replace(/[^\d.]/g, '');
+		return this.optional(element) || (num.length === 10);
+	}, 'Enter a valid phone number (123-456-7890).');
+
+	$('#addForm').validate({
+		onkeyup: false,
+		rules: {
+			name: 'required',
+			phone: {
+				validPhone: true
+			},
+			fax: {
+				validPhone: true
+			}
+		},
+		errorPlacement: function(error, element) {
+			error.insertAfter($(element));
+		},
+		highlight: function(element, errorClass, validClass) {
+			var con = $(element).parent().parent();
+			con.addClass(errorClass);
+
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			var con = $(element).parent().parent();
+			con.removeClass(errorClass);
+		},
+		errorElement: 'span'
+	});
+
+	$('input[name=phone], input[name=fax]').change(function() {
+		var txtbox = $(this);
+		var num = txtbox.val().replace(/[^\d.]/g, '');
+		var newval = '';
+		if (num.length == 0) {
+			newval = '';
+		}
+		else if (num.length < 4) {
+			newval = '(' + num;
+			if (num.length === 3)
+				newval = newval + ') ';
+		}
+		else if (num.length > 3 && num.length < 7) {
+			newval = '(' + num.substring(0, 3) + ') ' + num.substring(3);
+			if (num.length === 6)
+				newval = newval + ' - ';
+		}
+		else
+			newval = '(' + num.substring(0, 3) + ') ' + num.substring(3, 6) + ' - ' + num.substring(6);
+
+		txtbox.val(newval);
+	});
+
+});
+</script>
+
 
