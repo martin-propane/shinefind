b2667e4cb91be7befd842a250f8dabe2e27a0275
Martin Tribo <martin@propanemedia.com>
2013-03-13 15:52:17 -0500

    Made side info appear.

--- application/views/carwashes/index.blade.php
+++ application/views/carwashes/index.blade.php
@@ -46,7 +46,29 @@
         </div>
         <div id="location_box">
           <div>
-            <p><b>Wash Type:</b> Full Detail<br /><b>Attendant on Site:</b> Yes<br /><b>Vacuums:</b> N/A<br /><b>Wash Accessories Offered:</b> Yes<br /><b>Hours:</b> 8am-6pm<br /><b>Pricing</b> $50-$100</p>
+            <p>
+			<b>Wash Type: </b>
+			<?php
+			$OPTIONS = array('ConvenienceStore'=>'Convenience Store: ', 'CreditCards'=>'Accepts Credit Cards: ', 'Fuel'=>'Fuel: ', 'Fundraiser'=>'Fundraiser: ', 'GiftCards'=>'Gift Cards: ', 'MilitaryDiscount'=>'Military Discount: ', 'OilChange'=>'Oil Changes: ', 'PetWash'=>'Pet Wash: ', 'Salon'=>'Salon: ', 'SeniorDiscount'=>'Senior Discount: ');
+			$TYPE_NAMES = array('FullService'=>'Full Service', 'Tunnel'=>'Drive Through Tunnels', 'HandWash'=>'Manual Wash', 'Mobile'=>'Mobile Detail', 'Detailing'=>'Advanced Detailing', 'FreeVacuums'=>'Free Vacuums', 'SelfServe'=>'Self Serve', 'SoftTouch'=>'Soft Touch', 'TouchFree'=>'Touch Free', 'Xpress'=>'Express');
+			$found = false;
+			foreach ($TYPE_NAMES as $k=>$t)
+			{
+				if ($carwash->types[$k])
+				{
+					echo $t;
+					$found = true;
+					break;
+				}
+			}
+			if (!$found)
+				echo 'N/A';
+			?>
+			<br />
+			@foreach ($OPTIONS as $k=>$o)
+			<b>{{$o}}</b>{{ ($carwash->options[$k] == null) ? 'N/A' : 'Yes' }}<br />
+			@endforeach
+			</p>
           </div>
         </div><!--location box-->
         <div class="lt_col_ad2">
@@ -71,7 +93,7 @@
             </div>
             <div class="grid_4 alpha omega" id="addess">
               <p>{{ $carwash->busi_ad }}<br />{{ $carwash->city }}, {{ $carwash->state }} {{$carwash->zip}}<br />{{$carwash->phone}}<br />
-			  @if (isset($carwash->website))
+			  @if (isset($carwash->website) && $carwash->website != null)
 			  <a href="{{$carwash->website}}" target="_blank">&raquo;{{$carwash->website}}</a>
 			  @endif
 			  </p>
