6d03ca5176f7056044951e1518d5f5d17c11db68
Robert Smith <rgsmith1980@gmail.com>
2013-03-13 11:06:27 -0500

    Adjusted top menu to remove images. First pass at styling as text.

--- application/views/partials/top_menu.blade.php
+++ application/views/partials/top_menu.blade.php
@@ -1,11 +1,9 @@
     <div id="menu">
       <ul>
-        <li><a href="javascript:void(0)" onClick="showCityPopup();"><img src="{{ URL::to_asset('images/nav_city.jpg'); }}" width="160" height="44" border="0" /></a></li>
-        <li><a href="{{ URL::to_action('search/carwashes@index'); }}"><img src="{{ URL::to_asset('images/nav_detailers.jpg'); }}" width="123" height="44" border="0" /></a></li>
-        <li><a href="{{ URL::to('search/products') }}"><img src="{{ URL::to_asset('images/nav_diyprod.jpg'); }}" width="153" height="44" border="0" /></a></li>
-        <li><a href="{{URL::to('resources')}}"><img src="{{ URL::to_asset('images/nav_diyres.jpg'); }}" width="162" height="44" border="0" /></a></li>
-        <li><a href="{{URL::to('review')}}"><img src="{{ URL::to_asset('images/nav_review.jpg'); }}" width="169" height="44" border="0" /></a></li>
-        <li><div id="search_box"><input id="site_search" type="text" name="search" size="17" placeholder="Site Search"><a href="#"><img src="{{ URL::to_asset('images/search_button.png'); }}" align="right" /></a></div></li>
+        <li class="change-city"><a href="javascript:void(0)" onClick="showCityPopup();"></a></li>
+        <li><a href="{{ URL::to_action('search/carwashes@index'); }}">Detailers</a></li>
+        <li><a href="{{ URL::to('search/products') }}">DIY Products</a></li>
+        <li><a href="{{URL::to('review')}}">Write A Review</a></li>
       </ul>      
     </div> <!--end menu-->
 
