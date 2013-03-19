19d144a07880e06dbc2b0863284d6e41384fa8ae
Martin Tribo <martin@propanemedia.com>
2013-03-19 15:17:10 -0500

    Added useful links to top user bar when logged in.

--- application/views/partials/facebook.blade.php
+++ application/views/partials/facebook.blade.php
@@ -1,3 +1,8 @@
       <div id="facebook">
-        <a href="#"><img src="{{ URL::to_asset('images/facebook.png'); }}" width="302" height="33" /></a><p><a href="#">or create a login</a></p>
+	  @if (!Auth::check())
+        <a href="#"><img src="{{ URL::to_asset('images/facebook.png'); }}" width="302" height="33" /></a><p><a href="{{ URL::to('register'); }}">or create a login</a></p>
+	  @else
+	  Hello {{ Auth::user()->first_name }}<br />
+	  {{HTML::link('user/home', 'Member Home');}} {{HTML::link('logout', 'Log Out');}}
+	  @endif
       </div> <!--end facebook-->
